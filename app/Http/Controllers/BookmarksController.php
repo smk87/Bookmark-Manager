f705b701dbe95e138ed6eb172c5f00a320992233
smk87 <30999133+smk87@users.noreply.github.com>
2019-04-10 17:04:49 +0600

    First commit.

---
+++ app/Http/Controllers/BookmarksController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Bookmark;
+
+class BookmarksController extends Controller
+{
+    public function  __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('Home');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'url' => 'required'
+        ]);
+
+        // Create Bookmark
+        $bookmark = new Bookmark;
+
+        $bookmark->name = $request->input('name');
+        $bookmark->url = $request->input('url');
+        $bookmark->description = $request->input('description');
+        $bookmark->user_id = auth()->user()->id; // Getting currently logged in user's id
+
+        $bookmark->save();
+
+        return redirect('/home')->with('success', 'Bookmark Added.');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    @include('inc.messages')
+                    <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#addModal" type="button"
+                        name="=button">Add Bookmark</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal" tabindex="-1" role="dialog" id="addModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Bookmark</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('bookmarks.store') }}" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="">Bookmark Name</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Bookmark URLS</label>
+                        <input type="text" class="form-control" name="url">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Website Description</label>
+                        <textarea class="form-control" name="description"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success" value="Submit">
+                </form>
+            </div>
+            {{-- <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div> --}}
+        </div>
+    </div>
+</div>
+@endsection
